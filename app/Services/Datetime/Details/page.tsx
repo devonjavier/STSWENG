fcab8d966b615c24c7dd40396e86bc18cd310199
gitnadu <121154953+gitnadu@users.noreply.github.com>
2024-06-07 01:25:13 +0800

    Update page.tsx

--- app/Services/Datetime/Details/page.tsx
+++ app/Services/Datetime/Details/page.tsx
@@ -1,24 +1,62 @@
 'use client'
 import Link from 'next/link'
-import { createCustomer } from '@/app/lib/actions';
-import { useState } from 'react';
+import { Component, Dispatch, SetStateAction, useState } from 'react';
+import { useDebouncedCallback } from 'use-debounce';
+import GenerateDivs  from '@/app/components/GenerateDivs';
 
-const page = ({searchParams}:
-    {
-        searchParams:{
-            dates:string
-            timeslot1:string
-            timeslot2:string
-        }
+const page = ({searchParams}:{
+    searchParams: {
+        dates: string, // JSON
+        timeslot1: string,
+        timeslot2: string
     }
-) => {
+}) => {
+    
+    function getadditionalCustomers(count: number){
+        const additionalCustomers = []
+        // Update the customer array at the specified index
+        for (let i = 0; i < count; i++) {
+           const temp = document.getElementById(`additionalcustomer-${i}`);
+            additionalCustomers.push(temp);
+        }
+        
+        return additionalCustomers;
+    };
 
-  return (
+    const [isChecked, setIsChecked] = useState(false); // assume its false
+    const [maincustomername, setmaincustomername] = useState(" "); // assume no customer
+    const [additionalRequests, setadditionalRequests] = useState(" "); // assume no customer
+    const [additionalCustomers, setadditionalCustomers] = useState<string[]>([]);// assume no addtional customer
+
+    const handleCheckboxChange = (e:any) => {
+        setIsChecked(e.target.checked);
+        console.log(isChecked)
+    };
+
+    const mainCustomerNameChange = useDebouncedCallback((term) => {
+        setmaincustomername(term);
+    },300);
+
+    const addtionalRequestChange = useDebouncedCallback((additionalreq) => {
+        setadditionalRequests(additionalreq);
+    },300);
+
+    const [countAdditionalCustomers, setcountAdditionalCustomers] = useState(0);
+
+    function checker(dates:string){
+        var format;
+        format = JSON.parse(dates)
+        // console.log(format);
+        console.log(typeof format[0])
+    }
+
+    return (
     <>
     <div className='px-32 flex flex-col gap-8 mb-6 mt-20'>
         <div>
             <div className='text-cusBlue text-6xl font-bold'>
                 Book an Appointment
+                <button onClick={() => checker(searchParams.dates)}>checker</button>
             <div>
             </div>
             </div>
@@ -27,45 +65,75 @@ const page = ({searchParams}:
             </div>
         </div>
         <div className="flex flex-row">
-            <form action={createCustomer}>
+
             <div className='flex flex-row'>
                 <div className='flex flex-col'>
                      
                     <span className='text-cusBlue font-bold mb-5  text-lg'> Main Customer</span>
-                    <input id='customername' name='customername'      placeholder = "Name (Autofill)" className='text-cusBlue text-center text-2xl font-medium w-[480px] h-[68px] py-2.5 bg-white rounded-[20px] border border-indigo-800 justify-between items-center inline-flex' type="text" />
+                    <input id='customername' name='customername' placeholder = "Name" 
+                        onChange={(e)=>{
+                            mainCustomerNameChange(e.target.value);
+                        }}
+                    className='text-cusBlue text-center text-2xl font-medium w-[480px] h-[68px] py-2.5 bg-white rounded-[20px] border border-indigo-800 justify-between items-center inline-flex' type="text" />
                     
                     <div className='flex flex-col'>
                         <div className="flex flex-row mt-5 mb-3 items-center">
                             <span className='text-cusBlue font-bold  text-lg mr-16'> Additional Persons Involved </span>
                             
-                            <button className='rounded-full bg-cusBlue  w-[45px] h-[45px]'> - </button>
-                            <input placeholder = "#" className='text-cusBlue text-center text-2xl font-medium w-[100px] h-[68px] py-2.5 bg-white rounded-[20px] border border-indigo-800 justify-between items-center inline-flex' type="text" />
-                            <button className='rounded-full bg-cusBlue w-[45px] h-[45px]'> + </button>
+                            <button className='rounded-full bg-cusBlue  w-[45px] h-[45px]' onClick={() => setcountAdditionalCustomers(countAdditionalCustomers - 1)}> - </button>
+                            <span className='text-cusBlue font-bold mb-5  text-lg mx-4'> {countAdditionalCustomers} </span>
+                            <button className='rounded-full bg-cusBlue w-[45px] h-[45px]' onClick={() => setcountAdditionalCustomers(countAdditionalCustomers + 1)} > + </button>
+                            
+                            
                         </div>
-                        <input id='additionalname1' name='additionalname1' placeholder = "----" className='text-cusBlue text-center text-2xl font-medium w-[480px] h-[68px] py-2.5 my-4 bg-white rounded-[20px] border border-indigo-800 justify-between items-center inline-flex' type="text" />
-                        <input id='additionalname2' name='additionalname2' placeholder = "----" className='text-cusBlue text-center text-2xl font-medium w-[480px] h-[68px] py-2.5 my-4 bg-white rounded-[20px] border border-indigo-800 justify-between items-center inline-flex' type="text" />
-                        <input id='additionalname3' name='additionalname3' placeholder = "----" className='text-cusBlue text-center text-2xl font-medium w-[480px] h-[68px] py-2.5 my-4 bg-white rounded-[20px] border border-indigo-800 justify-between items-center inline-flex' type="text" />
 
+                        <GenerateDivs counter={countAdditionalCustomers} setadditionalCustomers={setadditionalCustomers} />
+                            
+                        {additionalCustomers.map((customer, i) => (
+                            <li key={i}> {customer}</li>
+                        ))}
                     </div>
 
-                    <Link href="/Services/Datetime/Details" type="submit">  <button  className="bg-cusBlue rounded-3xl w-56 h-11 mt-8 px-0 text-white font-bold"> Proceed to Confirmation </button> </Link>
+                    <Link 
+                    href={{
+                        pathname:"/Services/Datetime/Details/Confirmation",
+                        query: {
+                            dates: searchParams.dates,
+                            timeslot1: searchParams.timeslot1,
+                            timeslot2: searchParams.timeslot2,
+                            maincustomername: maincustomername,
+                            needsparking:isChecked,
+                            additionalrequests:additionalRequests,
+                            additionalCustomers:JSON.stringify(additionalCustomers)
+                        }
+                    }}>  
+                    
+                    <button className="bg-cusBlue rounded-3xl w-56 h-11 mt-8 px-0 text-white font-bold"> Proceed to Confirmation </button> </Link>
 
                 </div>
                 
                 <div className='flex flex-col ml-28'>
                     <div className='flex flex-col'>
                         <span className='text-cusBlue font-bold mb-2 text-lg'> Additional Request/s</span>
-                        <input id='additionalreq' name='additionalreq' placeholder = "----" className='text-cusBlue text-center text-2xl font-medium w-[480px] h-[250px] py-2.5 my-4 bg-white rounded-[20px] border border-indigo-800 justify-between items-center inline-flex' type="text" />
+                        <input id='additionalreq' name='additionalreq' 
+                            onChange={(e)=>{
+                                addtionalRequestChange(e.target.value);
+                            }}
+                        placeholder = " " className='text-cusBlue text-center text-2xl font-medium w-[480px] h-[250px] py-2.5 my-4 bg-white rounded-[20px] border border-indigo-800 justify-between items-center inline-flex' type="text" />
                     </div>
                     <div>
                         <span className='text-cusBlue font-bold mb-2 text-lg'> Do you need parking? </span>
-                        <input id='parking' name='parking' placeholder = "Yes/No" className='text-cusBlue text-center text-2xl font-medium w-[480px] h-[68px] py-2.5 bg-white rounded-[20px] border border-indigo-800 justify-between items-center inline-flex' type="text" />
+                        <input
+                            type="checkbox"
+                            checked={isChecked}
+                            onChange={handleCheckboxChange}
+                        />
                     </div>
 
                 </div>
 
             </div>
-            </form>
+
             
             <div>
 
