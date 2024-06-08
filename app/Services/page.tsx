0dd2cedb9536135b3997d70600227af96540a3ce
devon-javier <140371446+devon-javier@users.noreply.github.com>
2024-06-08 09:09:11 +0800

    Services page can differentiate between onetime service and hourly service, database needs update

--- app/Services/page.tsx
+++ app/Services/page.tsx
@@ -6,34 +6,74 @@ import { createClient } from '@/utils/supabase/server'
 export default async function displayPage() {
 
     const supabase = createClient();
-    const { data: services } = await supabase.from('Service').select()
-
+    const { data: services } = await supabase.from('Service').select();
+    const { data: onetimeServices } = await supabase.from('OnetimeService').select();
+    const { data: hourlyServices } = await supabase.from('HourlyService').select();
     // console.log(await supabase.from('Service').select());
 
+
+    const completeServices = services?.map(service => {
+        const onetimeservice = onetimeServices?.find(ot => ot.serviceid === service.id);
+        const hourlyservice = hourlyServices?.find(h => h.serviceid === service.id);
+
+        if(onetimeservice){
+            return({
+                service,
+                onetimeservice,
+                serviceType: 'onetime'
+            });
+        } else {
+            return({
+                service,
+                hourlyservice,
+                serviceType: 'hourly'
+            });
+        }
+    });
+
     return (
-      <>
-      <div className='px-32 flex flex-col gap-8 mb-6 mt-20'>
-      <div>
-          <div className='text-cusBlue text-6xl font-bold'>
-              Book an Appointment
-          </div>
-          <div>
-              <span className='text-cusBlue'>Package </span>&gt; Date & Time &gt; Details &gt; Confirmation
-          </div>
-      </div>
-      
-      <div className='grid grid-cols-2 gap-20 gap-y-10'>
-          {services ? (
-              services.map(service => (
-                <Link href="/Services/Datetime" key={service.serviceid}>
-                  <div><img className="w-full h-64 rounded-3xl shadow mb-5" src="" alt="" /></div>
-                  <div className='text-black text-3xl font-bold'>{service.title}</div>
-                  <div className='w-full text-cusBlue text-2xl font-light'><span className="font-bold">₱3500/3hr</span> in-person session with basic mix and mastering inclusion, where an...</div>
-                </Link>
-              ))
-            ) : (
-              <p>No services available. </p>
-            )}
+        <>
+        <div className='px-32 flex flex-col gap-8 mb-6 mt-20'>
+        <div>
+            <div className='text-cusBlue text-6xl font-bold'>
+                Book an Appointment
+            </div>
+            <div>
+                <span className='text-cusBlue'>Package </span>&gt; Date & Time &gt; Details &gt; Confirmation
+            </div>
+        </div>
+        
+        <div className='grid grid-cols-2 gap-20 gap-y-10'>
+        {completeServices ? (
+            completeServices.map(service => {
+
+            //able to differentiate between onetime and hourly altho needs testing
+
+            // not sure how to implement the rate within a description automatically though. 
+            
+            if(service.serviceType === 'onetime'){
+                return(
+                    <Link href="/Services/Datetime" key={service.service.serviceid}>
+                        <div><img className="w-full h-64 rounded-3xl shadow mb-5" src="" alt="" /></div>
+                        <div className='text-black text-3xl font-bold'>{service.service.title}</div>
+                        <div className='w-full text-cusBlue text-2xl font-light'><span className="font-bold">₱3500/3hr</span> in-person session with basic mix and mastering inclusion, where an...</div>
+                    </Link>
+                )
+            }
+
+            if(service.serviceType === 'hourly'){
+                return(
+                    <Link href="/Services/Datetime" key={service.service.serviceid}>
+                        <div><img className="w-full h-64 rounded-3xl shadow mb-5" src="" alt="" /></div>
+                        <div className='text-black text-3xl font-bold'>{service.service.title}</div>
+                        <div className='w-full text-cusBlue text-2xl font-light'><span className="font-bold">₱3500/3hr</span> in-person session with basic mix and mastering inclusion, where an...</div>
+                    </Link>
+                )
+            }
+            
+        })) : (
+            <p>No services available. </p>
+        )}
   
           {/* <Link href="/Services/Datetime">
               <div><img className = "w-full h-64 rounded-3xl shadow mb-5" src="" alt="" /></div>
@@ -56,14 +96,14 @@ export default async function displayPage() {
               <div className='w-full text-cusBlue text-2xl font-light'>Original beat production for <span className="font-bold">₱3000</span>.</div>
           </Link> */}
   
-      </div>
-      <div>
-          <Link className = "btn text-cusBlue text-2xl font-medium w-64 h-16 p-2.5 bg-white rounded-3xl border border-cusBlue justify-center items-center gap-2.5 inline-flex " href="">
-              View All
-          </Link>
-      </div>
-      </div>
-      </>
+        </div>
+        <div>
+            <Link className = "btn text-cusBlue text-2xl font-medium w-64 h-16 p-2.5 bg-white rounded-3xl border border-cusBlue justify-center items-center gap-2.5 inline-flex " href="">
+                View All
+            </Link>
+        </div>
+        </div>
+        </>
     )
 }
 
