0ed71921031a794dc35bfca213cce610141bc7e1
Luke <luke.m.regalado@gmail.com>
2024-06-27 03:34:08 +0800

    Pending reservations tweaks

--- app/(admin)/Admin/(dashboard)/pending-reservations/page.tsx
+++ app/(admin)/Admin/(dashboard)/pending-reservations/page.tsx
@@ -8,6 +8,17 @@ const Page = () => {
   return (
     <div className='px-32 flex flex-col gap-8 mb-6 mt-20'>
       <PendingCalendar setArrFunc={setSelectedDates} />
+
+      <div className="legend flex mt-0">
+          <div className="legend-item flex items-center mr-4">
+            <span className="bg-yellow-400 inline-block w-4 h-4 mr-2"></span>
+            <span className="font-bold text-black">Pending</span>
+          </div>
+          <div className="legend-item flex items-center">
+            <span className="bg-green-700 inline-block w-4 h-4 mr-2"></span>
+            <span className="font-bold text-black">Reserved</span>
+          </div>
+        </div>
     </div>
   );
 }
--- app/components/PendingCalendar.tsx
+++ app/components/PendingCalendar.tsx
@@ -9,6 +9,7 @@ const pendingDates = [
 ];
 
 const reservedDates = [
+  new Date(2024, 5, 23), // june 23, 2024
   new Date(2024, 6, 15), // july 15, 2024
   new Date(2024, 6, 16), // july 16, 2024
 ];
@@ -61,16 +62,6 @@ const PendingCalendar: React.FC<PendingCalendarProps> = ({ setArrFunc }) => {
             return false;
           }}
         />
-        <div className="legend flex mt-4">
-          <div className="legend-item flex items-center mr-4">
-            <span className="bg-yellow-400 inline-block w-4 h-4 mr-2"></span>
-            <span className="font-bold text-black">Pending</span>
-          </div>
-          <div className="legend-item flex items-center">
-            <span className="bg-green-700 inline-block w-4 h-4 mr-2"></span>
-            <span className="font-bold text-black">Reserved</span>
-          </div>
-        </div>
         {pendingDates.map(date => (
           <Link key={date.toDateString()} href={`/details?date=${date.toISOString()}`} legacyBehavior>
             <a className="date-link" style={{ display: 'none' }}>Pending: {date.toDateString()}</a>
