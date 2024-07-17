f42c0631449ea69dbd0a2444ef4de00aba317fa3
EdricJensenSee <125674757+EdricJensenSee@users.noreply.github.com>
2024-07-17 21:20:39 +0800

    BookingStatus Page matched

--- app/(home)/Services/Datetime/Details/Extradetails/Confirmation/Bookingstatus/page.tsx
+++ app/(home)/Services/Datetime/Details/Extradetails/Confirmation/Bookingstatus/page.tsx
@@ -158,23 +158,37 @@ const Page = ({searchParams}:{
                 Services &gt; Date & Time &gt; Details  &gt; Confirmation &gt;<span className='text-cusBlue'> Booking Status</span> 
             </div>
         </div>
-        <div className="flex flex-col items-center">
-            <Image src="/check_mark.png" alt="check-mark" width={150} height={130}/>
-            <span className="font-bold text-4xl text-black my-3"> Submission Successful </span>
-
-            <div className="flex flex-col border-2 border-black rounded-xl radius-md py-5 px-7 text-center mb-3">
-                <span className="text-2xl text-black"> Your Booking Reference Number is </span>
-                <span className="font-bold text-4xl text-black"> {trackingNumber} </span>
+        <div className="flex flex-row items-center justify-center w-4/5 m-auto">
+            <div className="self-start">
+                <Image src="/check_mark.png" alt="check-mark" width={500} height={220}/>
             </div>
-
+            <div className="flex flex-col items-center">
+                <div className="flex flex-col items-start self-start mb-5">
+                    <span className="font-bold text-6xl text-black self-start "> Submission Successful </span>
+                    <span className="font-bold text-2xl text-black"> Please <Link href = "/Checkstatus" className="text-cusBlue"> check status </Link> after a few hours for any updates </span>
+                </div>
+                <div className="flex flex-col border-2 border-black rounded-xl radius-md text-center mb-3 px-32 py-5 gap-6 w-full">
+                    <span className="text-2xl text-black font-bold"> Your Booking Reference Number is </span>
+                    <span className="font-bold text-5xl text-black"> {trackingNumber} </span>
+                </div>
+                <div className="flex flex-col border-2 border-black rounded-xl radius-md text-start mb-3 px-32 py-5 gap-6">
+                    <span className="text-3xl text-black font-bold text-start"> Reminders: </span>
+                    <span className="ml-8 font-semibold text-xl text-black"> Please note that your reservation will only be approved once the down payment is made. Kindly send proof of payment through any of our social media channels. </span>
+                    <span>
+                        <div className="ml-8 font-semibold text-xl text-black">Instagram: @indigostudiosph</div>
+                        <div className="ml-8 font-semibold text-xl text-black">Facebook: facebook.com/indigostudiosph</div>
+                    </span>
+                </div>
             <div>
-                <span className="font-bold text-black"> Please <span className="text-cusBlue"> check status </span> after a few hours for any updates </span>
             </div>
             <Link 
                     href={{
                         pathname:"/",
                     }}>  
-            <button className="bg-cusBlue rounded-3xl w-56 h-11 mt-8 px-0 text-white font-bold"> Back to Home </button> </Link>
+            <button className="bg-cusBlue btn hover:bg-indigo-900 rounded-3xl w-56 mt-8 h-20 text-xl text-white font-bold"> Back to Home </button> </Link>
+            </div>
+    
+
         </div>
         
     </div>
