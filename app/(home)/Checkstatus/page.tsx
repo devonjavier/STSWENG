0de9e4d49830cc1f763a568f76095dfa18434c37
EdricJensenSee <125674757+EdricJensenSee@users.noreply.github.com>
2024-07-17 22:19:25 +0800

    CheckStatus Page matched

--- app/(home)/Checkstatus/page.tsx
+++ app/(home)/Checkstatus/page.tsx
@@ -8,6 +8,11 @@ import { useDebouncedCallback } from 'use-debounce';
 import Image from "next/image"
 
 
+const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+};
+
 
 export default function DisplayPage() {
 
@@ -131,11 +136,11 @@ export default function DisplayPage() {
 
         
         
-    }, [trackingNumber]); // if the tracking number changes
+    }, [trackingNumber]); 
 
     return (
         <>
-            <div className='flex flex-col mt-16 ml-16'>
+            <div className='flex flex-col mt-16 ml-48'>
             <span className='text-cusBlue font-bold mb-2 mt-6  text-7xl'> Check Status </span>
             <span className='text-gray-400 font-bold mb-6 text-xl'> Track your appointment here. </span>
             <span className='text-black font-bold mb-2  text-xl'> Reference Number: </span>
@@ -146,67 +151,68 @@ export default function DisplayPage() {
                         className='text-cusBlue text-center text-2xl font-medium w-[480px] h-[68px] py-2.5 bg-white rounded-[20px] border border-indigo-800 justify-between items-center inline-flex' type="text" />
             <button className="bg-cusBlue rounded-3xl w-56 h-11 mt-8 px-0 text-white font-bold" onClick={() => getStatus()}> Check status </button>
             </div>
+            <div className='m-5'>
+            <hr />
+            <hr />
+            <hr />
+            </div>
+
             <div className='flex flex-col items-center'>
                 <Image src={status} alt=" " width={80} height={80}/>
-                <span className="font-bold text-4xl text-black my-3"> {statusMessage} </span>
+                <span className="font-bold text-4xl text-black"> {statusMessage} </span>
             </div>
             
-            <div className="flex flex-row mx-96 mt-5 mb-6 border-2 p-5">
-                <div className='flex flex-row'>
-                    <div className='flex flex-col'>
-                        <div className="flex flex-col ">
-                            <span className='text-cusBlue font-bold mb-5  text-3xl'> Main Customer: </span>
+            <div className="flex flex-row justify-center mb-6 p-5 w-2/3 m-auto">
+                <div className='flex flex-row w-full'>
+                    <div className='flex flex-col w-full'>
+                        <div className="flex flex-col border-2 border-indigo-800 mt-5 rounded-lg p-4 drop-shadow-2xl">
+                            <span className='text-black  font-bold mb-5  text-3xl'> Main Customer: </span>
                             <div className='flex flex-col p-0 ml-12 my-0 mr-0'>
-                                <span className='text-cusBlue font-bold mb-5  text-xl mar '> First name: {maincustomerfirstname} </span>
-                                <span className='text-cusBlue font-bold mb-5  text-xl'> Middle name: {maincustomermiddlename} </span>
-                                <span className='text-cusBlue font-bold mb-5  text-xl'> Last name: {maincustomerlastname} </span>
-                                <span className='text-cusBlue font-bold mb-5  text-xl'> Contact number: {phonenumber} </span>
-                                <span className='text-cusBlue font-bold mb-5  text-xl'> Email addres: {emailaddress} </span>
+                                <span className='text-black font-bold mb-5  text-xl mar '> Name: {maincustomerfirstname} {maincustomermiddlename} {maincustomerlastname}</span>
+                                <span className='text-black font-bold mb-5  text-xl'> Contact number: {phonenumber} </span>
+                                <span className='text-black font-bold mb-5  text-xl'> Email addres: {emailaddress} </span>
                             </div>
-                            
                         </div>
 
-                        <div className='flex flex-col'>
-                            <span className='text-cusBlue font-bold  text-3xl mr-16 my-6'> Additional Persons Involved: </span>
-                            <div className='flex flex-col p-0 ml-5 my-0 mr-0'>
-                            
-                            {additionalCustomersFirstname.map((firstName:string, i:number) => (
-                                <span className='text-cusBlue text-xl font-bold mb-5 ml-7 text-md' key={i}>
-                                    Person {i+1}. {firstName} {additionalCustomersMiddlename[i]} {additionalCustomersLastname[i]}
-                                </span>
-                            ))}
-                            
-                            </div>
-                            
-                        </div>
+              <div className='flex flex-col border-2 border-indigo-800 mt-5 rounded-lg p-4 drop-shadow-2xl'>
+                <span className='text-black font-bold text-3xl mr-16 my-6'> Additional Persons Involved: </span>
+                <div className='flex flex-col p-0 my-0 mr-0'>
+                  {additionalCustomersFirstname.length === 0 ? (
+                    <span className='text-black text-xl font-semibold mb-5 ml-8 text-md'>None</span>
+                  ) : (
+                    additionalCustomersFirstname.map((firstName, i) => (
+                      <span className='text-black text-xl font-semibold mb-5 ml-8 text-md' key={i}>
+                         Person {i+1}. {firstName} {additionalCustomersMiddlename[i]} {additionalCustomersLastname[i]}
+                      </span>
+                    ))
+                  )}
+                </div>
+              </div>
 
-                        <div className='flex flex-col'>
-                            <span className='text-cusBlue font-bold  text-2xl mr-16 my-6'> Parking Spot: {isChecked ? "Will need" : "Will not need"} </span>
+                        <div className='flex flex-col border-2 border-indigo-800 mt-5 rounded-lg p-4 drop-shadow-2xl'>
+                            <span className='text-black  font-bold mb-5  text-3xl'> Parking Spot: {isChecked ? "Yes" : "No"} </span>
                         </div>
                         
                     </div>
                     
-                    <div className='flex flex-col ml-28'>
-                        <span className='text-cusBlue font-bold mb-2 text-2xl'> Reservation Details: </span>
-                        <span className='text-cusBlue font-bold mb-5 ml-7  text-md'> Package Selection:: {selectedService}  </span>
-                        <span className='text-cusBlue font-bold mb-2 ml-7  text-md'> Appointment schedules: </span>
+                    <div className='flex flex-col border-2 border-indigo-800 mt-5 rounded-lg p-4 drop-shadow-2xl ml-4 w-full'>
+                        <span className='text-black  font-bold mb-5  text-3xl'> Reservation Details: </span>
+                        <span className='text-black font-bold mb-5 ml-7  text-md'> Package Selection:: {selectedService}  </span>
+                        <span className='text-black font-bold mb-2 ml-7  text-md'> Appointment schedules: </span>
                         <div className='flex flex-col mb-4'>
                             {listofschedules.map((schedule) => (
-                                    <span key={schedule.scheduleid} className='text-black font-bold mb-0 ml-12  text-md'> Date: {schedule.date} {schedule.starttime} - {schedule.endtime} </span>
+                                    <span key={schedule.scheduleid} className='text-black font-bold  mb-5 ml-12  text-md'> Date: {formatDate(schedule.date)} 
+                                                                    <div>
+                                        Time: {schedule.starttime} - {schedule.endtime}
+                                        </div>
+                                    </span>
                             ))}
                         </div>
-                        
-            
-                        <span className='text-cusBlue font-bold mb-5 ml-7  text-md'> Additional Requests:  </span>
+                        <span className='text-black font-bold mb-5 ml-7  text-md'> Additional Requests:  </span>
                         <div className="flex flex-col ml-16">
-                            <span className='text-cusBlue font-bold mb-5 text-md'> {additionalRequests} </span>
+                            <span className='text-black font-bold mb-5 text-md'> {additionalRequests} </span>
                         </div>
-
                     </div>
-
-                </div>
-                <div>
-
                 </div>
             </div>
         
