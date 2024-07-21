5818d936d5eb5bb15416becc743fa1084ed722fc
EdricJensenSee <125674757+EdricJensenSee@users.noreply.github.com>
2024-07-22 02:08:34 +0800

    CheckStatus visibility Changes

--- app/(home)/Checkstatus/page.tsx
+++ app/(home)/Checkstatus/page.tsx
@@ -1,4 +1,3 @@
-
 'use client'
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react';
@@ -7,63 +6,46 @@ import { service } from '@/utils/supabase/interfaces'
 import { useDebouncedCallback } from 'use-debounce';
 import Image from "next/image"
 
-
 const formatDate = (dateString) => {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     return new Date(dateString).toLocaleDateString(undefined, options);
 };
 
-
 export default function DisplayPage() {
 
     const [trackingNumber, setTrackingNumber] = useState<number>(12) 
-    
-    const [isChecked, setIsChecked] = useState(); // assume its false
-
-    const [selectedService, setSelectedService] = useState();
-
+    const [isChecked, setIsChecked] = useState(false); // assume its false
+    const [selectedService, setSelectedService] = useState("");
     const [maincustomerfirstname, setmaincustomerfirstname] = useState(" "); // assume no customer
     const [maincustomermiddlename, setmaincustomermiddlename] = useState(" "); // assume no customer
     const [maincustomerlastname, setmaincustomerlastname] = useState(" "); // assume no customer
-
     const [phonenumber, setPhonenumber] = useState(" "); // assume no customer
     const [emailaddress, setEmailaddress] = useState(" "); // assume no customer
-
     const [additionalRequests, setadditionalRequests] = useState(" "); // assume no request\
-
     const [additionalCustomersFirstname, setadditionalCustomersFirstname] = useState<string[]>([]);// assume no addtional customer
     const [additionalCustomersMiddlename, setadditionalCustomersMiddlename] = useState<string[]>([]);// assume no addtional customer
     const [additionalCustomersLastname, setadditionalCustomersLastname] = useState<string[]>([]);// assume no addtional customer
-
     const [listofschedules,setlistofschedules] = useState<[]>([])
-
     const [status, setStatus] = useState<string>("/check_mark.png");
     const [statusMessage , setStatusMessage] = useState<string>(" ");
+    const [isContentVisible, setIsContentVisible] = useState(false);
 
     const trackingNumberChange = ((term:number) => {
         setTrackingNumber(term);
     });
 
-    const getStatus = async() =>{
+    const fetchStatus = async() =>{
 
-        
         const getThatAppointment = await fetchOneAppointment(trackingNumber);
-        //console.log(getThatAppointment[0].appointmentid); // this is the appointmentid
-
         const status = getThatAppointment[0].status
 
-        if(status === "Pending")
-        {
+        if(status === "Pending") {
             setStatus("/pending_mark.png");
             setStatusMessage("Appointment Pending");
-        }
-        else if(status === "Accepted")
-        {
+        } else if(status === "Accepted") {
             setStatus("/check_mark.png");
             setStatusMessage("Appointment Accepted");
-        }
-        else if(status === "Rejected")
-        {
+        } else if(status === "Rejected") {
             setStatus("/ekis_mark.png");
             setStatusMessage("Appointment Rejected");
         }
@@ -74,18 +56,12 @@ export default function DisplayPage() {
         const mainCustomer = await fetchOneCustomer(appid, true);
         
         setadditionalRequests(getThatAppointment[0].additionalrequest);
-
         setIsChecked(getThatAppointment[0].isparkingspotneeded);
-        //console.log(mainCustomer[0].personid);
 
         const selectedservice = await fetchOneService(parseInt(getThatAppointment[0].serviceid));
-                //console.log(selectedservice[0].title);
         setSelectedService(selectedservice[0].title);
 
-
         const mainCustomerDetails = await fetchOnePerson(mainCustomer[0].personid);
-        console.log(mainCustomerDetails[0].firstname);
-
         setmaincustomerfirstname(mainCustomerDetails[0].firstname);
         setmaincustomermiddlename(mainCustomerDetails[0].middlename)
         setmaincustomerlastname(mainCustomerDetails[0].lastname);
@@ -93,22 +69,17 @@ export default function DisplayPage() {
         setEmailaddress(mainCustomerDetails[0].emailaddress);
 
         const additionalCustomers = await fetchOneCustomer(appid, false);
-        //const additionalCustomersDetails = await fetchMultiplePerson(additionalCustomers);
         
         let additionalcust  = []
         let additionalfirstnames = []
         let additionalmiddlenames = []
         let additionallastnames = []
 
-
         for(let detail of additionalCustomers){
-
             additionalcust = await fetchOnePerson(detail.personid);
-
             additionalfirstnames.push(additionalcust[0].firstname);
             additionalmiddlenames.push(additionalcust[0].middlename);
             additionallastnames.push(additionalcust[0].lastname);
-
         }
 
         setadditionalCustomersFirstname(additionalfirstnames);
@@ -116,112 +87,110 @@ export default function DisplayPage() {
         setadditionalCustomersLastname(additionallastnames);
 
         const newSchedules:number[] = [];
-
         const getData = await fetchSelectedSchedule(appid);
-        //const date = new Date(new Date(selectedsched.date).setDate(new Date(selectedsched.date).getDate() + 1)).toISOString()
         
         getData.forEach((one: number) => {
-            
-            console.log(one);
             newSchedules.push(one);
-            console.log(newSchedules);
-            
         });
 
         setlistofschedules(newSchedules);
-        //console.log(listofschedules)
+
+        setTimeout(() => {
+            setIsContentVisible(true);
+        }, 0); 
     }
-    
 
-    
-    useEffect(() => {
+    const getStatus = async() => {
+        if (isContentVisible) {
+            setIsContentVisible(false);
+            setTimeout(fetchStatus, 300); 
+        } else {
+            fetchStatus();
+        }
+    }
 
-        
-        
-    }, [trackingNumber]); 
+    useEffect(() => {}, [trackingNumber]); 
 
     return (
         <>
             <div className='flex flex-col mt-16 ml-48'>
-            <span className='text-cusBlue font-bold mb-2 mt-6  text-7xl'> Check Status </span>
-            <span className='text-gray-400 font-bold mb-6 text-xl'> Track your appointment here. </span>
-            <span className='text-black font-bold mb-2  text-xl'> Reference Number: </span>
-            <input placeholder = "Input reference number" 
-                            onChange={(e)=>{
-                                trackingNumberChange(parseInt(e.target.value));
-                            }}
-                        className='text-cusBlue text-center text-2xl font-medium w-[480px] h-[68px] py-2.5 bg-white rounded-[20px] border border-indigo-800 justify-between items-center inline-flex' type="text" />
-            <button className="bg-cusBlue rounded-3xl w-56 h-11 mt-8 px-0 text-white font-bold" onClick={() => getStatus()}> Check status </button>
+                <span className='text-cusBlue font-bold mb-2 mt-6  text-7xl'> Check Status </span>
+                <span className='text-gray-400 font-bold mb-6 text-xl'> Track your appointment here. </span>
+                <span className='text-black font-bold mb-2  text-xl'> Reference Number: </span>
+                <input placeholder = "Input reference number" 
+                    onChange={(e)=>{
+                        trackingNumberChange(parseInt(e.target.value));
+                    }}
+                    className='text-cusBlue text-center text-2xl font-medium w-[480px] h-[68px] py-2.5 bg-white rounded-[20px] border border-indigo-800 justify-between items-center inline-flex' type="text" />
+                <button className="bg-cusBlue rounded-3xl w-56 h-11 mt-8 px-0 text-white font-bold" onClick={() => getStatus()}> Check status </button>
             </div>
-            <div className='m-5'>
-            <hr />
-            <hr />
-            <hr />
-            </div>
+            {isContentVisible && ( 
+                <>
+                    <div className='m-5'>
+                        <hr />
+                        <hr />
+                        <hr />
+                    </div>
 
-            <div className='flex flex-col items-center'>
-                <Image src={status} alt=" " width={80} height={80}/>
-                <span className="font-bold text-4xl text-black"> {statusMessage} </span>
-            </div>
-            
-            <div className="flex flex-row justify-center mb-6 p-5 w-2/3 m-auto">
-                <div className='flex flex-row w-full'>
-                    <div className='flex flex-col w-full'>
-                        <div className="flex flex-col border-2 border-indigo-800 mt-5 rounded-lg p-4 drop-shadow-2xl">
-                            <span className='text-black  font-bold mb-5  text-3xl'> Main Customer: </span>
-                            <div className='flex flex-col p-0 ml-12 my-0 mr-0'>
-                                <span className='text-black font-bold mb-5  text-xl mar '> Name: {maincustomerfirstname} {maincustomermiddlename} {maincustomerlastname}</span>
-                                <span className='text-black font-bold mb-5  text-xl'> Contact number: {phonenumber} </span>
-                                <span className='text-black font-bold mb-5  text-xl'> Email address: {emailaddress} </span>
-                            </div>
-                        </div>
-
-              <div className='flex flex-col border-2 border-indigo-800 mt-5 rounded-lg p-4 drop-shadow-2xl'>
-                <span className='text-black font-bold text-3xl mr-16 my-6'> Additional Persons Involved: </span>
-                <div className='flex flex-col p-0 my-0 mr-0'>
-                  {additionalCustomersFirstname.length === 0 ? (
-                    <span className='text-black text-xl font-semibold mb-5 ml-12 text-md'>None</span>
-                  ) : (
-                    additionalCustomersFirstname.map((firstName, i) => (
-                      <span className='text-black text-xl font-semibold mb-5 ml-12 text-md' key={i}>
-                         Person {i+1}. {firstName} {additionalCustomersMiddlename[i]} {additionalCustomersLastname[i]}
-                      </span>
-                    ))
-                  )}
-                </div>
-              </div>
-
-                        <div className='flex flex-col border-2 border-indigo-800 mt-5 rounded-lg p-4 drop-shadow-2xl'>
-                            <span className='text-black  font-bold mb-5  text-3xl'> Parking Spot: {isChecked ? "Yes" : "No"} </span>
-                        </div>
-                        
+                    <div className='flex flex-col items-center'>
+                        <Image src={status} alt=" " width={80} height={80}/>
+                        <span className="font-bold text-4xl text-black"> {statusMessage} </span>
                     </div>
                     
-                    <div className='flex flex-col border-2 border-indigo-800 mt-5 rounded-lg p-4 drop-shadow-2xl ml-4 w-full'>
-                        <span className='text-black  font-bold mb-5  text-3xl'> Reservation Details: </span>
-                        <span className='text-black font-bold mb-5 ml-7  text-2xl'> Package Selection: {selectedService}  </span>
-                        <span className='text-black font-bold mb-2 ml-7  text-2xl'> Appointment schedules: </span>
-                        <div className='flex flex-col mb-4'>
-                            {listofschedules.map((schedule) => (
-                                    <span key={schedule.scheduleid} className='text-black font-bold  mb-5 ml-12  text-md'> Date: {formatDate(schedule.date)} 
-                                                                    <div>
-                                        Time: {schedule.starttime} - {schedule.endtime}
-                                        </div>
-                                    </span>
-                            ))}
-                        </div>
-                        <span className='text-black font-bold mb-5 ml-7  text-md'> Additional Requests:  </span>
-                        <div className="flex flex-col ml-16">
-                            <span className='text-black font-bold mb-5 text-md'> {additionalRequests} </span>
+                    <div className="flex flex-row justify-center mb-6 p-5 w-2/3 m-auto">
+                        <div className='flex flex-row w-full'>
+                            <div className='flex flex-col w-full'>
+                                <div className="flex flex-col border-2 border-indigo-800 mt-5 rounded-lg p-4 drop-shadow-2xl">
+                                    <span className='text-black  font-bold mb-5  text-3xl'> Main Customer: </span>
+                                    <div className='flex flex-col p-0 ml-12 my-0 mr-0'>
+                                        <span className='text-black font-bold mb-5  text-xl mar '> Name: {maincustomerfirstname} {maincustomermiddlename} {maincustomerlastname}</span>
+                                        <span className='text-black font-bold mb-5  text-xl'> Contact number: {phonenumber} </span>
+                                        <span className='text-black font-bold mb-5  text-xl'> Email address: {emailaddress} </span>
+                                    </div>
+                                </div>
+
+                                <div className='flex flex-col border-2 border-indigo-800 mt-5 rounded-lg p-4 drop-shadow-2xl'>
+                                    <span className='text-black font-bold text-3xl mr-16 my-6'> Additional Persons Involved: </span>
+                                    <div className='flex flex-col p-0 my-0 mr-0'>
+                                        {additionalCustomersFirstname.length === 0 ? (
+                                            <span className='text-black text-xl font-semibold mb-5 ml-12 text-md'>None</span>
+                                        ) : (
+                                            additionalCustomersFirstname.map((firstName, i) => (
+                                                <span className='text-black text-xl font-semibold mb-5 ml-12 text-md' key={i}>
+                                                    Person {i+1}. {firstName} {additionalCustomersMiddlename[i]} {additionalCustomersLastname[i]}
+                                                </span>
+                                            ))
+                                        )}
+                                    </div>
+                                </div>
+
+                                <div className='flex flex-col border-2 border-indigo-800 mt-5 rounded-lg p-4 drop-shadow-2xl'>
+                                    <span className='text-black  font-bold mb-5  text-3xl'> Parking Spot: {isChecked ? "Yes" : "No"} </span>
+                                </div>
+                            </div>
+                            
+                            <div className='flex flex-col border-2 border-indigo-800 mt-5 rounded-lg p-4 drop-shadow-2xl ml-4 w-full'>
+                                <span className='text-black  font-bold mb-5  text-3xl'> Reservation Details: </span>
+                                <span className='text-black font-bold mb-5 ml-7  text-2xl'> Package Selection: {selectedService}  </span>
+                                <span className='text-black font-bold mb-2 ml-7  text-2xl'> Appointment schedules: </span>
+                                <div className='flex flex-col mb-4'>
+                                    {listofschedules.map((schedule) => (
+                                        <span key={schedule.scheduleid} className='text-black font-bold  mb-5 ml-12  text-md'> Date: {formatDate(schedule.date)} 
+                                            <div>
+                                                Time: {schedule.starttime} - {schedule.endtime}
+                                            </div>
+                                        </span>
+                                    ))}
+                                </div>
+                                <span className='text-black font-bold mb-5 ml-7  text-md'> Additional Requests:  </span>
+                                <div className="flex flex-col ml-16">
+                                    <span className='text-black font-bold mb-5 text-md'> {additionalRequests} </span>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        
+                </>
+            )}
         </>
     )
 }
-
-
-
-
