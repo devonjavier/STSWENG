2687bcb60155e2a5d00a67e962ac505462c164e1
angieolonan03 <giastkidding@gmail.com>
2024-10-10 04:20:54 +0800

    Added css code to darken bg images in page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -11,7 +11,8 @@ import { createClient } from '@/utils/supabase/server'
 export default async function Home() {
   return (
     <>
-    <div className="bg-cover bg-center h-screen flex flex-col justify-end relative pb-16 pl-8 lg:pb-32 lg:pl-16 mx-auto " style={{ backgroundImage: "url('/home_bg.jpg')" }}> 
+    <div className="relative bg-cover bg-center h-screen flex flex-col justify-end pb-16 pl-8 lg:pb-32 lg:pl-16 mx-auto" style={{ backgroundImage: "url('/home_bg.jpg')" }}>
+      <div className="absolute inset-0 bg-black opacity-50"></div> {/* Darkened the image bg */}
       <div className='lg:pt-96 lg:pl-20'>  
         <IntroText />
       </div>
@@ -21,7 +22,7 @@ export default async function Home() {
         <Services />
       </div>
     </div>
-
+      
     <div className='bg-white pt-5 lg:p-14 h-max flex flex-col justify-center relative pb-10 mb-0 mx-auto w-max'>
       <Team />
     </div>
@@ -29,7 +30,8 @@ export default async function Home() {
     <div className='hidden lg:block flex justify-center lg:pb-20'>
       <Reviews />
     </div>
-    <div className="bg-cover bg-center h-screen flex justify-center items-center" style={{ backgroundImage: "url('/home_collage.png')"}}>
+    <div className="relative bg-cover bg-center h-screen flex justify-center items-center" style={{ backgroundImage: "url('/home_collage.png')"}}>
+    <div className="absolute inset-0 bg-black opacity-50"></div>
       <HearTheDifferences />
     </div>
     </>
