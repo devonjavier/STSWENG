342713d58a5b0119134c71025d8cc0704f6adb7d
jumangie <82877810+angieolonan03@users.noreply.github.com>
2024-10-09 21:15:20 +0800

    Added highlights of the active link in navbar in NavBar.tsx

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,13 +3,18 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React, { useState } from 'react'
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+import { usePathname } from 'next/navigation'
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false)
+  const pathname = usePathname();
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen)
   }
+
+  const isActiveLink = (path: string) => pathname === path
+  
   return (
     <nav className="relative z-50 bg-white py-2 px-4 shadow-md">
       <div className="flex justify-between items-center">
@@ -35,19 +40,19 @@ const NavBar = () => {
 
         {/* Navigation Links */}
         <div className={`hidden md:flex md:flex-1 justify-start space-x-8 text-cusBlue text-sm md:text-md font-medium`}>
-          <Link href="/" className="hover:text-gray-400 transition duration-300 ease-in-out delay-300">
+          <Link href="/" className={`transition duration-300 ease-in-out delay-300 ${isActiveLink('/') ? 'text-[#7E01B0]' : 'hover:text-gray-400'}`}>
             Home
           </Link>
-          <Link href="/Services" className="hover:text-gray-400 transition duration-300 ease-in-out delay-300">
+          <Link href="/Services" className={`transition duration-300 ease-in-out delay-300 ${isActiveLink('/Services') ? 'text-[#7E01B0]' : 'hover:text-gray-400'}`}>
             Services
           </Link>
-          <Link href="/About" className="hover:text-gray-400 transition duration-300 ease-in-out delay-300">
+          <Link href="/About" className={`transition duration-300 ease-in-out delay-300 ${isActiveLink('/About') ? 'text-[#7E01B0]' : 'hover:text-gray-400'}`}>
             About
           </Link>
-          <Link href="/FAQs" className="hover:text-gray-400 transition duration-300 ease-in-out delay-300">
+          <Link href="/FAQs" className={`transition duration-300 ease-in-out delay-300 ${isActiveLink('/FAQs') ? 'text-[#7E01B0]' : 'hover:text-gray-400'}`}>
             FAQs
           </Link>
-          <Link href="/Checkstatus" className="hover:text-gray-400 transition duration-300 ease-in-out delay-300">
+          <Link href="/Checkstatus" className={`transition duration-300 ease-in-out delay-300 ${isActiveLink('/Checkstatus') ? 'text-[#7E01B0]' : 'hover:text-gray-400'}`}>
             Check Status
           </Link>
         </div>
