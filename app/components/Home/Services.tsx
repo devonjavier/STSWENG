58bed1cf741c3075c91ee7fd164352553fe9f8ad
angieolonan03 <giastkidding@gmail.com>
2024-10-10 15:01:31 +0800

    Updated Services.tsx UI to make it more responsive

--- app/components/Home/Services.tsx
+++ app/components/Home/Services.tsx
@@ -13,52 +13,53 @@ const Services = () => {
       </div>
       
       <div className="container mx-auto flex justify-center items-center h-auto pt-48 pb-4 overflow-hidden font-atyp">
-        <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           
-        <div className="bg-black bg-opacity-50 rounded-xl transition-transform duration-300 hover:scale-105" style={{ height: '33rem', width: '17rem', display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: '2rem' }}>
-          <div className='text-white text-sm lg:text-xl font-semibold text-center'>Regularly<br />Hourly Rate<br /></div>
-          <div className='text-[#D3D3D3] text-4xs lg:text-xs font-light text-center pt-2'>Experience top-quality recording<br />& sound production with professional<br />mix & mastering.<br /><br /></div>
-          <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 underline text-left w-full'>What&apos;s Included:<br /></div>
-          <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 text-left w-full'>&gt; Studio session at ₱1,000 per hour.<br /></div>
-          <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 text-left w-full'>&gt; Mix and mastering services for an<br />  additional ₱2,500.<br /><br /><br /><br /></div>
-          <div className='text-[#D3D3D3] text-2xs lg:text-base font-light text-center pt-2'>Starts From<br /></div>
-          <div className='text-white text-4xl lg:text-3xl font-bold text-center pt-2'>₱1,000/hour<br /><br /><br /></div>
-          <a href="/Services"><button className="bg-[#4B27A8] hover:bg-gray-300 text-white font-medium py-2 px-4 rounded-full transition duration-300 ease-in-out delay-300">Get Started</button></a>
-        </div>
+          <div className="bg-black bg-opacity-50 rounded-xl transition-transform duration-300 hover:scale-105" style={{ minHeight: '33rem', minWidth: '17rem', display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: '2rem' }}>
+            <div className='text-white text-sm lg:text-xl font-semibold text-center'>Regularly<br />Hourly Rate<br /></div>
+            <div className='text-[#D3D3D3] text-4xs lg:text-xs font-light text-center pt-2'>Experience top-quality recording<br />& sound production with professional<br />mix & mastering.<br /><br /></div>
+            <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 underline text-left w-full'>What&apos;s Included:<br /></div>
+            <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 text-left w-full'>&gt; Studio session at ₱1,000 per hour.<br /></div>
+            <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 text-left w-full'>&gt; Mix and mastering services for an<br />  additional ₱2,500.<br /><br /><br /><br /></div>
+            <div className='text-[#D3D3D3] text-2xs lg:text-base font-light text-center pt-2'>Starts From<br /></div>
+            <div className='text-white text-4xl lg:text-3xl font-bold text-center pt-2'>₱1,000/hour<br /><br /></div>
+            <a href="/Services"><button className="bg-[#4B27A8] hover:bg-gray-300 text-white font-medium py-2 px-4 rounded-full transition duration-300 ease-in-out delay-300">Get Started</button></a>
+            
+          </div>
 
-        <div className="bg-black bg-opacity-50 rounded-xl transition-transform duration-300 hover:scale-105" style={{ height: '33rem', width: '17rem', display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: '2rem' }}>
-          <div className='text-white text-sm lg:text-xl font-semibold text-center'>RAW<br />Audio Recording</div>
-          <div className='text-[#D3D3D3] text-4xs lg:text-xs font-light text-center pt-2'>Capture high-quality audio, perfect for<br />clients who handle their own<br />editing & engineering.<br /><br /></div>
-          <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 underline text-left w-full'>What&apos;s Included:</div>
-          <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 text-left w-full'>&gt; High-quality raw audio recording.<br /></div>
-          <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 text-left w-full'>&gt; Tailored for those working with their<br />  own sound engineers and editors.<br /><br /><br /><br /></div>
-          <div className='text-[#D3D3D3] text-2xs lg:text-base font-light text-center pt-2'>Starts From<br /></div>
-          <div className='text-white text-4xl lg:text-3xl font-bold text-center pt-2'>₱1,500/hour<br /><br /><br /></div>
-          <a href="/Services"><button className="bg-[#4B27A8] hover:bg-gray-300 text-white font-medium py-2 px-4 rounded-full transition duration-300 ease-in-out delay-300">Get Started</button></a>
-        </div>
+          <div className="bg-black bg-opacity-50 rounded-xl transition-transform duration-300 hover:scale-105" style={{ minHeight: '33rem', minWidth: '17rem', display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: '2rem' }}>
+            <div className='text-white text-sm lg:text-xl font-semibold text-center'>RAW<br />Audio Recording</div>
+            <div className='text-[#D3D3D3] text-4xs lg:text-xs font-light text-center pt-2'>Capture high-quality audio, perfect for<br />clients who handle their own<br />editing & engineering.<br /><br /></div>
+            <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 underline text-left w-full'>What&apos;s Included:</div>
+            <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 text-left w-full'>&gt; High-quality raw audio recording.<br /></div>
+            <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 text-left w-full'>&gt; Tailored for those working with their<br />  own sound engineers and editors.<br /><br /><br /><br /></div>
+            <div className='text-[#D3D3D3] text-2xs lg:text-base font-light text-center pt-2'>Starts From<br /></div>
+            <div className='text-white text-4xl lg:text-3xl font-bold text-center pt-2'>₱1,500/hour<br /><br /></div>
+            <a href="/Services"><button className="bg-[#4B27A8] hover:bg-gray-300 text-white font-medium py-2 px-4 rounded-full transition duration-300 ease-in-out delay-300">Get Started</button></a>
+          </div>
 
-        <div className="bg-black bg-opacity-50 rounded-xl transition-transform duration-300 hover:scale-105" style={{ height: '33rem', width: '17rem', display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: '2rem' }}>
-          <div className='text-white text-sm lg:text-xl font-semibold text-center'>Original<br />Beat Production</div>
-          <div className='text-[#D3D3D3] text-4xs lg:text-xs font-light text-center pt-2'>Get custom beats tailored to your<br />style, with prices adjusted based<br />on complexity.<br /><br /></div>
-          <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 underline text-left w-full'>What&apos;s Included:</div>
-          <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 text-left w-full'>&gt; Custom beat production.<br /></div>
-          <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 text-left w-full'>&gt; Price adjustments for complex<br />  productions.<br /><br /><br /><br /></div>
-          <div className='text-[#D3D3D3] text-2xs lg:text-base font-light text-center pt-2'>Starts From<br /></div>
-          <div className='text-white text-4xl lg:text-3xl font-bold text-center pt-2'>₱3,000/session<br /><br /><br /></div>
-          <a href="/Services"><button className="bg-[#4B27A8] hover:bg-gray-300 text-white font-medium py-2 px-4 rounded-full transition duration-300 ease-in-out delay-300">Get Started</button></a>
-        </div>
+          <div className="bg-black bg-opacity-50 rounded-xl transition-transform duration-300 hover:scale-105" style={{ minHeight: '33rem', minWidth: '17rem', display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: '2rem' }}>
+            <div className='text-white text-sm lg:text-xl font-semibold text-center'>Original<br />Beat Production</div>
+            <div className='text-[#D3D3D3] text-4xs lg:text-xs font-light text-center pt-2'>Get custom beats tailored to your<br />style, with prices adjusted based<br />on complexity.<br /><br /></div>
+            <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 underline text-left w-full'>What&apos;s Included:</div>
+            <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 text-left w-full'>&gt; Custom beat production.<br /></div>
+            <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 text-left w-full'>&gt; Price adjustments for complex<br />  productions.<br /><br /><br /><br /></div>
+            <div className='text-[#D3D3D3] text-2xs lg:text-base font-light text-center pt-2'>Starts From<br /></div>
+            <div className='text-white text-4xl lg:text-3xl font-bold text-center pt-2'>₱3,000/session<br /><br /></div>
+            <a href="/Services"><button className="bg-[#4B27A8] hover:bg-gray-300 text-white font-medium py-2 px-4 rounded-full transition duration-300 ease-in-out delay-300">Get Started</button></a>
+          </div>
 
-        <div className="bg-black bg-opacity-50 rounded-xl transition-transform duration-300 hover:scale-105" style={{ height: '33rem', width: '17rem', display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: '2rem' }}>
-          <div className='text-white text-sm lg:text-xl font-semibold text-center'>Online Advanced<br />Mix & Mastering</div>
-          <div className='text-[#D3D3D3] text-4xs lg:text-xs font-light text-center pt-2'>Polish your tracks with advanced mix<br />& mastering with free minor revisions<br />& disocunts for multiple tracks.<br /><br /></div>
-          <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 underline text-left w-full'>What&apos;s Included:</div>
-          <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 text-left w-full'>&gt; Free minor revisions.<br /></div>
-          <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 text-left w-full'>&gt; Discounts on 2 or more tracks<br />  (based on track difficulty).<br /></div>
-          <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 text-left w-full'>&gt; Major revisions at an additional ₱200.<br /><br /><br /></div>
-          <div className='text-[#D3D3D3] text-2xs lg:text-base font-light text-center pt-2'>Starts From<br /></div>
-          <div className='text-white text-4xl lg:text-3xl font-bold text-center pt-2'>₱2,800/session<br /><br /><br /></div>
-          <a href="/Services"><button className="bg-[#4B27A8] hover:bg-gray-300 text-white font-medium py-2 px-4 rounded-full transition duration-300 ease-in-out delay-300">Get Started</button></a>
-        </div>
+          <div className="bg-black bg-opacity-50 rounded-xl transition-transform duration-300 hover:scale-105" style={{ minHeight: '33rem', minWidth: '17rem', display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: '2rem' }}>
+            <div className='text-white text-sm lg:text-xl font-semibold text-center'>Online Advanced<br />Mix & Mastering</div>
+            <div className='text-[#D3D3D3] text-4xs lg:text-xs font-light text-center pt-2'>Polish your tracks with advanced mix<br />& mastering with free minor revisions<br />& discounts for multiple tracks.<br /><br /></div>
+            <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 underline text-left w-full'>What&apos;s Included:</div>
+            <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 text-left w-full'>&gt; Free minor revisions.<br /></div>
+            <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 text-left w-full'>&gt; Discounts on 2 or more tracks<br />  (based on track difficulty).<br /></div>
+            <div className='text-white text-4xs lg:text-xs font-light pt-2 pl-8 text-left w-full'>&gt; Major revisions at an additional ₱200.<br /><br /><br /></div>
+            <div className='text-[#D3D3D3] text-2xs lg:text-base font-light text-center pt-2'>Starts From<br /></div>
+            <div className='text-white text-4xl lg:text-3xl font-bold text-center pt-2'>₱2,800/session<br /><br /></div>
+            <a href="/Services"><button className="bg-[#4B27A8] hover:bg-gray-300 text-white font-medium py-2 px-4 rounded-full transition duration-300 ease-in-out delay-300">Get Started</button></a>
+          </div>
 
         </div>
       </div>
